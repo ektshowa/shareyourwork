0a255350f10c899b0a662bb3b5a27a97d07587e4
Etienne Tshowa <ektshowa@gmail.com>
2014-06-18 22:06:06 -0400

    Finish sign in

---
+++ factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name        "Etienne Tshowa"
+    email       "etienne@yahoo.com"
+    password    "foobar"
+    password_confirmation "foobar"
+  end
+end
